825d8b5b4f5a558513c0576d3ad381a0c2360a1a
Jared Walters <jared.w.walters@gmail.com>
2017-04-02 10:17:34 -0700

    Remove arbitrary comments

--- src/HappyNum.js
+++ src/HappyNum.js
@@ -1,8 +1,3 @@
-// 1. Given a number of undetermined length (1, 12, 123, 4567897)
-// 2. only deal with single digit at a time
-// 3. square (multiply the number by itself) each number
-// 4. add the sum of all nums in a given string of nums together
-
 // Given a number return array containing each digit of the number
 function singleDigits(num) {
   return num
