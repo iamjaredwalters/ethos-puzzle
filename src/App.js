f1861b7dabe648e73057cd001662422b655ecca2
Jared Walters <jared.w.walters@gmail.com>
2017-04-03 19:17:54 -0700

    Format code

--- src/App.js
+++ src/App.js
@@ -6,52 +6,51 @@ import totalSum from './HappyNum.js';
 let total = 0;
 
 const isHappy = (num) => {
-  let sum = num;
-  let run = true;
+    let sum = num;
+    let run = true;
 
-  /**
-   * Sum the numbers
-   *  If sum === 1 then keep that number, its happy
-   *  If sum === 4 that number is no good, move on to next num
-   */
-  while (run) {
+    /**
+     * Sum the numbers
+     *  If sum === 1 then keep that number, its happy
+     *  If sum === 4 that number is no good, move on to next num
+     */
+    while (run) {
 
-    // Number is happy
-    if (sum === 1) {
-      total += num;
+        // Number is happy
+        if (sum === 1) {
+            total += num;
+        }
+
+        // Exit loop if sum is 1 or 4
+        if (sum === 1 || sum === 4) {
+            run = false;
+        }
+
+        sum = totalSum(sum);
     }
-
-    // Exit loop if sum is 1 or 4
-    if (sum === 1 || sum === 4) {
-      run = false;
-    }
-
-    sum = totalSum(sum);
-  }
 }
 
 
 // Note: this needs to start at 1 not 0
 for (let i = 1; i <= 1000000; i++) {
-  isHappy(i);
+    isHappy(i);
 }
 
 
-
 class App extends Component {
-  render() {
-    return (
+    render() {
+        return (
             <div className="App">
-              <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h2>Welcome to React Hi</h2>
-              </div>
-              <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-              </p>
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React Hi</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
             </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
